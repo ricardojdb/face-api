92f427e59ffb1ca31964010415b6e7ee062bd7cf
deepai <rjoseb11@gmail.com>
2019-03-25 14:59:26 -0500

    Added small demo with webcam

---
+++ demo.py
@@ -0,0 +1,110 @@
+from datetime import datetime, timedelta
+import numpy as np
+import requests
+import base64
+import cv2
+import sys
+import os
+
+import utils.utils as utils
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def encode_img(img):
+    _, buffer = cv2.imencode('.jpg', img)
+    img_str = base64.b64encode(buffer)
+    return img_str
+
+table_dict = {}
+
+video_path = 0 
+video_capture = utils.WebcamVideoStream(video_path).start()
+ 
+while True:
+    h, w = int(video_capture.h), int(video_capture.w)
+    ret, image = video_capture.read()
+    
+    if not ret: 
+        print('No frames has been grabbed') 
+        break
+    
+    img = np.copy(image)
+    
+    try:
+        detect_req = requests.get('http://192.168.8.100:7000/predict/', data=encode_img(img))
+        detections = detect_req.json()
+    except:
+        detections = []
+
+    data_list = []
+    for face in detections:
+        # extract the confidence (i.e., probability) associated with the prediction
+        confidence = face["confidence"]
+        
+        # filter out weak detections by ensuring the `confidence` is greater than the minimum confidence
+        if confidence > 0.3:
+            # compute the (x, y)-coordinates of the bounding box for the object
+            box =  face["box"] * np.array([w, h, w, h])
+            (xmin, ymin, xmax, ymax) = box.astype("int")
+
+            # Fix negatives and big numbers
+            xmin, ymin, xmax, ymax = max(xmin,0), max(ymin,0), min(xmax,w-1), min(ymax, h-1)
+
+            # Expand the bounding box to capture the entire head not only the face
+            # this improve the performance of gender, age and sentiment models
+            xmin_wide, ymin_wide, xmax_wide, ymax_wide = utils.get_wide_box(w, h, xmin, ymin, xmax, ymax)
+
+            # Extract Face from image with (x,y)-coordinates
+            roi_color = img[max(ymin,0):min(ymax,h-1), max(xmin,0):min(xmax,w-1)]
+            roi_color_wide = img[ymin_wide:ymax_wide, xmin_wide:xmax_wide]
+
+            if not roi_color.size: continue
+            
+            # Predict age and gender
+            try:
+                agen_req = requests.get('http://192.168.8.100:7002/predict/', data=encode_img(roi_color_wide))
+                agen_predict = agen_req.json()
+                gender, age = agen_predict["gender"], agen_predict["age"]
+            except:
+                gender, age = " ", 0
+            
+            # Emotion Scores
+            try:
+                emot_req = requests.get('http://192.168.8.100:7001/predict/', data=encode_img(roi_color))            
+                scores = emot_req.json()["emotions"]
+            except:
+                scores = [1,0,0,0,0,0,0,0]            
+            
+            # Facial Recognition
+            try:
+                recog_req = requests.get('http://192.168.8.100:7003/predict/', data=encode_img(roi_color_wide))
+                recog = recog_req.json()
+                fr_score, label = recog["dist"], recog["label"]
+            except:
+                fr_score, label = 0, " "
+
+            time_stamp = datetime.now().strftime("%H:%M:%S")
+            
+            # Add label to Dataset with (Name, Main Sentiment, Gender, Last Seen(Time))
+            if label in table_dict:
+                age_ewa = utils.weighted_average(age, table_dict[label][2], beta=0.998)
+                scores_ewa = utils.weighted_average(scores, table_dict[label][3:11], beta=0.8)
+                box_ewa = utils.weighted_average([xmin, ymin, xmax, ymax], 
+                                                 table_dict[label][-2], beta=0.998)
+                table_dict[label] = [label, gender, age_ewa, *scores_ewa, box_ewa, time_stamp]
+            else:
+                table_dict[label] = [label, gender, age, *scores, [xmin, ymin, xmax, ymax], time_stamp]
+
+            data_list.append(table_dict[label])
+            
+    video_capture.data_list = data_list
+        
+    if video_capture.stopped:
+        break
+
+cv2.destroyAllWindows()
+
+
+
+
---
+++ utils/utils.py
@@ -0,0 +1,165 @@
+from threading import Thread
+import numpy as np
+import traceback
+import time
+import six
+import sys
+import cv2
+import os
+
+colors = {'anger':(0,0,190), 'disgust':(0,184,113),'fear': (98,24,91), 
+          'happiness':(8, 154, 255), 'sadness':(231,217,0), 'surprise':(0, 253, 255), 
+          'neutral':(200,200,200), 'contempt':(200,200,200)}
+
+classes = ['neutral', 'happiness', 'surprise', 'sadness', 'anger', 'disgust', 'fear', 'contempt']
+
+def get_wide_box(w, h, xmin, ymin, xmax, ymax):
+    xmin_wide = max(xmin-(xmax-xmin)//4, 0)
+    ymin_wide = max(ymin-(ymax-ymin)//4, 0)
+    xmax_wide = min(xmax+(xmax-xmin)//6, w-1)
+    ymax_wide = min(ymax+(ymax-ymin)//6, h-1)
+    return xmin_wide, ymin_wide, xmax_wide, ymax_wide
+
+def weighted_average(Vdw, dw, beta):
+    Vdw = np.asarray(Vdw)
+    dw = np.asarray(dw)
+    return beta * Vdw + (1-beta) * dw
+
+def draw_box(image, label, gender, age, scores, classes, colors, box):
+    """ Draws a Bounding Box over a Face.
+    Args:
+        image (narray): the image containng the face
+        label (str): the label that goes on top of the box
+        scores (narray): Facial expression prediciton scores 
+        [xmin,ymin,xmax,ymax] (int): Bounding box coordinates
+    Return:
+        result_image (narray): edited image
+    """
+    xmin, ymin, xmax, ymax = np.array(box, dtype=int)
+    
+    img = np.copy(image)
+    output = np.copy(image)
+
+    fontType = cv2.FONT_HERSHEY_DUPLEX
+    alpha = 0.7
+
+    fontScale_box = 0.5
+    thickness_box = 1
+    fontScale = 0.4
+    thickness = 1
+    
+    label = "{} | {}, {}".format(label, gender, int(age))
+    
+    text_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_DUPLEX, fontScale_box, thickness_box)
+    center = (xmin+5, ymin - text_size[0][1]//2)
+    pt2 = (xmin + text_size[0][0] +10, ymin)
+    
+    box_color = (180,0,0)
+    # Rectangle around text
+    cv2.rectangle(img, (xmin, ymin - text_size[0][1]*2), pt2, box_color, 2)
+    cv2.rectangle(img, (xmin, ymin - text_size[0][1]*2), pt2, box_color, -1)
+    # Face rectangle
+    cv2.rectangle(img,(xmin,ymin),(xmax,ymax),box_color,2)
+
+    img = cv2.addWeighted(img, alpha, output, 1-alpha,0,output)
+    # Draw text
+    cv2.putText(img, label, center, cv2.FONT_HERSHEY_DUPLEX, fontScale_box, (255, 255, 255), thickness_box)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    for i, score in enumerate(scores):
+        bar_label = classes[i] + ' {}%'.format(int(scores[i]*100))
+        bar_text_size = cv2.getTextSize(bar_label, fontType, fontScale, thickness)
+        # Emotion bars
+        bar_x = xmax + 20
+        bar_y = ymin + bar_text_size[0][1] + (i*bar_text_size[0][1]*3)
+        bar_w = int(70 * score)
+        
+        bar_xmax = min(bar_x + bar_text_size[0][0], img.shape[1]-1)
+        bar_ymax = min(bar_y + bar_text_size[0][1], img.shape[0]-1)
+
+        bar_xmin = min(bar_x, img.shape[1]-1)
+        bar_ymin = min(bar_y, img.shape[0]-1)
+
+
+        font_color = (255,255,255)
+
+        if np.mean(gray[bar_ymin:bar_ymax,bar_xmin:bar_xmax]) > 100: font_color = (0,0,0)
+
+        cv2.putText(img, bar_label, (bar_x, bar_y), fontType, fontScale, font_color, thickness)
+        cv2.line(img, (bar_x+5, bar_y+ (bar_text_size[0][1])),(bar_x+5+bar_w, bar_y + (bar_text_size[0][1])), colors[classes[i]],int(fontScale*20))
+    
+    return img
+
+
+class WebcamVideoStream:
+    def __init__(self, src=0):#,model='yolo'
+        # initialize the video camera stream and read the first frame from the stream
+        self.stream = cv2.VideoCapture(src)
+        # Change depending on the resolution of the camera
+        # self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, 1920) 
+        # self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        self.h = self.stream.get(4)
+        self.w =self.stream.get(3)
+        (self.grabbed, self.frame) = self.stream.read()
+
+        self.data_list = None
+        # initialize the variable used to indicate if the thread should be stopped
+        self.stopped = False
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        self.stopped = False
+        self.thread = Thread(target=self.update, name='camera:0', args=())
+        self.thread.start()
+        return self
+ 
+    def update(self):
+        cv2.namedWindow('final_image', cv2.WINDOW_NORMAL)
+        # keep looping infinitely until the thread is stopped
+        while True:
+            try:
+                # if the thread indicator variable is set, stop the thread
+                if self.stopped:
+                    self.stream.release()
+                    cv2.destroyAllWindows()
+                    return
+
+                # otherwise, read the next frame from the stream
+                (self.grabbed, self.frame) = self.stream.read()
+                self.h = self.stream.get(4)
+                self.w =self.stream.get(3)
+                img = np.copy(self.frame)
+                
+                if not self.grabbed:
+                    print('No frames')
+                    self.stop()
+                    self.stream.release()
+                    cv2.destroyAllWindows()
+                    return
+
+                if self.data_list != None:
+                    for data in self.data_list:
+                        scores_dic = {c:s*100 for c,s in zip(classes, data[3:11])}
+                        
+                        img = draw_box(img, data[0], data[1], data[2], 
+                                        data[3:11], classes, colors, data[-2])
+
+                cv2.imshow('final_image',img)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    self.stream.release()
+                    cv2.destroyAllWindows()
+                    self.stop()
+                    return
+                
+            except Exception as e:
+                traceback.print_exc(file=sys.stdout)
+                return
+ 
+    def read(self):
+        # return the frame most recently read
+        return (self.grabbed, self.frame)
+ 
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
